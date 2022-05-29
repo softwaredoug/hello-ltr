8bd7948bd8885abc150301d38c01d58a6e6e259b
Doug Turnbull <softwaredoug@gmail.com>
2022-05-29 09:40:56 -0400

    Save rerank USE at 50

--- notebooks/elasticsearch/vmware/search.py
+++ notebooks/elasticsearch/vmware/search.py
@@ -96,7 +96,7 @@ def max_passage_rerank_at_5(client, query):
 
 
 def sum_passage_rerank_at_5(client, query):
-    """Rerank top 5 submissions by max passage USE similarity. NDCG of 0.30562"""
+    """Rerank top 5 submissions by max passage USE similarity. NDCG of 0.30550"""
     es = client.es
     body = {
         'size': 5,
@@ -138,6 +138,53 @@ def sum_passage_rerank_at_5(client, query):
     hits = sorted(hits, key=lambda x: x['_source']['sum_sim'], reverse=True)
     hits = hits[:5]
     return hits
+
+
+def max_passage_rerank_at_50(client, query):
+    """Rerank top 50 submissions by max passage USE similarity"""
+    es = client.es
+    body = {
+        'size': 50,
+        'query': {
+            'bool': { 'should': [
+                {'match_phrase': {
+                    'remaining_lines': {
+                        'slop': 10,
+                        'query': query
+                    }
+                }},
+                {'match_phrase': {
+                    'first_line': {
+                        'slop': 10,
+                        'query': query
+                    }
+                }},
+                {'match': {
+                    'raw_text': {
+                        'query': query
+                    }
+                }},
+                {'match': {
+                    'first_line': {
+                        'query': query
+                    }
+                }},
+            ]}
+        }
+    }
+
+    print(json.dumps(body, indent=2))
+
+    hits = es.search(index='vmware', body=body)['hits']['hits']
+
+    for hit in hits:
+        hit['_source']['max_sim'], hit['_source']['sum_sim'] = passage_similarity(query, hit, verbose=False)
+
+    hits = sorted(hits, key=lambda x: x['_source']['max_sim'], reverse=True)
+    hits = hits[:5]
+    return hits
+
+
 
 
 
