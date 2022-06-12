f6ded481332d860d267238d22d57699bf99a407a
Doug Turnbull <softwaredoug@gmail.com>
2022-06-12 14:38:46 -0400

    Save (failed) attempt at universe probabilities

--- notebooks/elasticsearch/vmware/simulate/simulate.py
+++ notebooks/elasticsearch/vmware/simulate/simulate.py
@@ -1,6 +1,8 @@
 # Can we infer relevance grades from just a difference in the mean NDCG of two samples?
 import pandas as pd
 import numpy as np
+import os
+import pickle
 import random
 from statistics import NormalDist
 
@@ -9,11 +11,11 @@ from statistics import NormalDist
 # NDCG at 5
 ndcgs = {
     'data/use_feedback_rrf_turnbull_submission_1653226391886872.csv': 0.16806,
-    'data/pull_out_firstline_turnbull_1653253060074837.csv': 0.29668,
     'data/turnbull_submission_1652544680901428.csv': 0.20911,
-    'data/with_best_compounds_at_50_plus_10_times_use_turnbull_165445182567455.csv': 0.32681,
-    'data/with_best_compounds_at_5_only_phrase_search_turnbull_1654439995765457.csv': 0.31643,
+    'data/pull_out_firstline_turnbull_1653253060074837.csv': 0.29668,
     'data/rerank_slop_search_remaining_lines_max_snippet_at_5_turnbull_1654439885030507.csv': 0.31574,
+    'data/with_best_compounds_at_5_only_phrase_search_turnbull_1654439995765457.csv': 0.31643,
+    'data/with_best_compounds_at_50_plus_10_times_use_turnbull_165445182567455.csv': 0.32681,
     # 'data/simulated_labels_turnbull.csv': 0.26169,
     # 'data/simulated_labels_turnbull_2.csv': 0.28722,
 
@@ -77,6 +79,11 @@ def add_weights(results):
 def create_results_diff(results_before, results_after):
     """Compute the DCG delta weights resulting from the before and after,
        so we can compare to observed mean delta dcg."""
+
+    results_before = results_before.groupby('QueryId').head(5)
+    results_after = results_after.groupby('QueryId').head(5)
+    assert len(results_before) == len(results_after)
+
     results_before = add_weights(results_before)
     results_after = add_weights(results_after)
     results = results_before.merge(results_after,
@@ -98,9 +105,9 @@ def create_results_diff(results_before, results_after):
     return results
 
 
-def universe_probability(actual_dcg_delta, simulated_dcg_delta, sigma=1000):
-    actual_universe_distribution = NormalDist(mu=actual_dcg_delta, sigma=sigma)
-    simulated_universe_distribution = NormalDist(mu=simulated_dcg_delta, sigma=sigma)
+def universe_probability(actual_dcg_delta, simulated_dcg_delta, std_dev=1000):
+    actual_universe_distribution = NormalDist(mu=actual_dcg_delta, sigma=std_dev)
+    simulated_universe_distribution = NormalDist(mu=simulated_dcg_delta, sigma=std_dev)
     universe_prob = actual_universe_distribution.overlap(simulated_universe_distribution)
 
     return universe_prob
@@ -121,7 +128,8 @@ def universe_probability(actual_dcg_delta, simulated_dcg_delta, sigma=1000):
 #
 #
 #
-def simulate_at(results, prob_positive, actual_dcg_delta, rounds=10, verbose=False):
+def simulate_at(results, prob_positive, actual_dcg_delta, rounds=10, verbose=False,
+                dcg_diff_std_dev=20, universe_prob_threshold=0.5):
     """Simulate by giving each doc moved up a grade of 1 with provided probabiility."""
     best_universe_prob = 0.0
     best_prob_positive = prob_positive
@@ -136,6 +144,7 @@ def simulate_at(results, prob_positive, actual_dcg_delta, rounds=10, verbose=Fal
 
         results['grade'] = 0
         results['grade_changed'] = False
+        results['actual_dcg_delta'] = actual_dcg_delta
         results.loc[results['weight_delta'] > 0, 'grade'] = rand_grades_positive
         results.loc[results['weight_delta'] < 0, 'grade'] = rand_grades_negative
         results.loc[results['weight_delta'] > 0, 'grade_changed'] = True
@@ -143,28 +152,24 @@ def simulate_at(results, prob_positive, actual_dcg_delta, rounds=10, verbose=Fal
 
         # DCG delta of this simulated universe - how close is it to the observed DCG delta?
         simulated_dcg_delta = sum(results['grade'] * results['weight_delta'])
-        universe_prob = universe_probability(actual_dcg_delta, simulated_dcg_delta)
-
-        # Measure how much the the actual / simulated distributions overlap
-        # to figure out how 'real' the simulated one is
-        # Increment alpha and beta in proportion to probability of the universe being real
-        results.loc[(results['grade'] == 1) & (results['weight_delta'] != 0), 'alpha'] += universe_prob
-        results.loc[(results['grade'] == 0) & (results['weight_delta'] != 0), 'beta'] += universe_prob
+        universe_prob = universe_probability(actual_dcg_delta, simulated_dcg_delta,
+                                             std_dev=dcg_diff_std_dev)
 
         # Move probability of drawing positive (the prob weights > 0 have relevance=1) in
         # a direction closer to making the simulated relevance universe more likely
+        tight_universe_prob = universe_probability(actual_dcg_delta, simulated_dcg_delta, std_dev=2)
         delta = actual_dcg_delta - simulated_dcg_delta
-        update = 0.01 * (1 - (universe_prob**0.0000001)) * sign(delta)
+        update = 0.01 * (1 - (tight_universe_prob**0.000000001)) * sign(delta)
         prob_positive += update
 
         if universe_prob > best_universe_prob:
             best_universe_prob = universe_prob
             best_prob_positive = prob_positive
 
-        if universe_prob > 0.5:
+        if universe_prob > universe_prob_threshold:
             universe = results.copy()
             universe = universe.groupby(['QueryId', 'DocumentId']).first()
-            universe['probability'] = universe_prob
+            universe['score'] = universe_prob
             universes.append(universe)
 
         if verbose:
@@ -172,18 +177,17 @@ def simulate_at(results, prob_positive, actual_dcg_delta, rounds=10, verbose=Fal
             msg += f"| Upd {update}, Draw {prob_positive:.3f} | Best {best_prob_positive:.3f} {best_universe_prob:.3f}"
             print(msg)
 
-    universes = sorted(universes, key=lambda x: x['probability'].iloc[0], reverse=True)
-    # Scale alpha and beta to not be overconfident by just having more rounds,
-    # scale to 1/10th of the number of rounds. 1/10th is an arbitrary number :)
-    # Alternatively, we could check whether we're encountering highly similar universes and discount
-    # by that similarity, but that would be complex and expensive
-    results['alpha'] /= (rounds / 10)
-    results['beta'] /= (rounds / 10)
+    universes = sorted(universes, key=lambda x: x['score'].iloc[0], reverse=True)
 
     return universes
 
 
-def intersect_universes(new_universes, universes, actual_dcg_delta):
+def is_duplicate_universe(universe1, universe2):
+    """Remove duplicate universes"""
+    return universe1.groupby(['QueryId', 'DocumentId'])['grade'] == universe2.groupby(['QueryId', 'DocumentId'])['grade']
+
+
+def intersect_universes(new_universes, universes, dcg_diff_std_dev=20, max_overlap=5000):
     """The best set of relevance grades corresponds to those where more diffs have accurate relevance grades
        so we want to get the intersection of all of the universes where we have good grades."""
     if universes == []:
@@ -191,37 +195,68 @@ def intersect_universes(new_universes, universes, actual_dcg_delta):
 
     intersected_universes = []
 
+    print(f"Intersecting {len(universes)} universes")
+
     # What's the probability of new universes grades for existing universes delta?
-    for universe in universes:
+    for old_universe in universes:
         for new_universe in new_universes:
-            intersected_universe = universe.merge(new_universe,
-                                                  on=['QueryId', 'DocumentId'],
-                                                  how='left',
-                                                  suffixes=('', '_new'))
-            # New universes has nothing to say on this, so we'll just use the old universe
+            actual_dcg_delta = old_universe['actual_dcg_delta'].iloc[0]
+            assert (old_universe['actual_dcg_delta'] == actual_dcg_delta).all()
 
-            assert (intersected_universe['grade_changed_new'].isna()
-                    == intersected_universe['grade_new'].isna()).all()
-            intersected_universe['grade_changed_new'] = intersected_universe['grade_changed_new'].fillna(False)
-            changed_grades = intersected_universe['grade_changed_new']
-            intersected_universe.loc[~changed_grades, 'grade_new'] = intersected_universe[~changed_grades]['grade']
-            assert not (intersected_universe['grade_new'].isna()).all()
-            simulated_dcg_delta = sum(intersected_universe['grade_new'] * intersected_universe['weight_delta'])
+            old_with_new_grades = old_universe.merge(new_universe,
+                                                     on=['QueryId', 'DocumentId'],
+                                                     how='left',
+                                                     suffixes=('', '_new'))
 
-            new_universe_prob = universe_probability(actual_dcg_delta, simulated_dcg_delta)
-            # print(f"Prob old {universe['probability'].iloc[0]:.3f} new {new_universe_prob:.3f}")
-            intersected_universe['probability'] *= new_universe_prob
-            intersected_universe.loc[changed_grades, 'grade_changed'] = True
-            intersected_universe = intersected_universe.loc[:, ~intersected_universe.columns.str.contains("_new$")].columns
+            # New universes has nothing to say on this, so we'll just use the old universe grades
+            assert (old_with_new_grades['grade_changed_new'].isna()
+                    == old_with_new_grades['grade_new'].isna()).all()
+            old_with_new_grades['grade_changed_new'] = old_with_new_grades['grade_changed_new'].fillna(False)
+            changed_grades = old_with_new_grades['grade_changed_new']
+            old_with_new_grades.loc[~changed_grades, 'grade_new'] = old_with_new_grades[~changed_grades]['grade']
+            assert not (old_with_new_grades['grade_new'].isna()).all()
 
-            intersected_universes.append(intersected_universe)
+            # When the two universes have large and similar search spaces,
+            # its difficult to find a good intersection, because the optimum for each is likely
+            # to be heavily overfitted
+            #
+            # We dont update their relative probabilitiy
+            overlapping_space = old_with_new_grades[
+                (old_with_new_grades['weight_delta'] != 0) & old_with_new_grades['grade_changed_new']
+            ][['weight_delta', 'grade_changed_new', 'grade', 'grade_new']]
 
-    intersected_universes = sorted(intersected_universes, key=lambda x: x['probability'].iloc[0], reverse=True)
+            if len(overlapping_space) < max_overlap:
+                simulated_dcg_delta = sum(old_with_new_grades['grade_new'] * old_with_new_grades['weight_delta'])
 
-    # Scale all universes to probability of 1
-    max_universe_prob = intersected_universes[0]['probability'].iloc[0]
-    for universe in intersected_universes:
-        universe['probability'] /= max_universe_prob
+                new_universe_prob = universe_probability(actual_dcg_delta, simulated_dcg_delta, std_dev=dcg_diff_std_dev)
+                print(f"Ovlap {len(overlapping_space)} Old {old_universe['score'].iloc[0]:.3f} Comb {new_universe_prob:.5f} | "
+                      f"{simulated_dcg_delta:.3f} {actual_dcg_delta:.3f}")
+                old_with_new_grades['score'] *= new_universe_prob
+                # old_with_new_grades = old_with_new_grades.loc[old_with_new_grades['grade_changed'], :].copy()
+            else:
+                print(f"Not intersecting with {len(overlapping_space)} overlapping examples")
+
+            # old_with_new_grades.loc[changed_grades, 'grade_changed'] = True
+            old_with_new_grades = old_with_new_grades.loc[:, ~old_with_new_grades.columns.str.contains("_new$")]
+            intersected_universes.append(old_with_new_grades)
+
+    intersected_universes = sorted(intersected_universes, key=lambda x: x['score'].iloc[0], reverse=True)
+
+    # Scale all universes to probability score of 1
+    # max_universe_prob = intersected_universes[0]['score'].iloc[0]
+    # if max_universe_prob > 0:
+    #    for universe in intersected_universes:
+    #        universe['score'] /= max_universe_prob
+
+    # Dedup universes
+    # deduped_universes = []
+    # for universe1 in intersected_universes:
+    #    for universe2 in intersected_universes:
+    #        if is_duplicate_universe(universe1, universe2):
+    #            print("Skiiping duplicate universe")
+    #            continue
+    #        deduped_universes.append(universe1)
+    # intersected_universes = deduped_universes
 
     # How many universes are unique?
     print("Num universes:", len(intersected_universes))
@@ -233,14 +268,14 @@ def main():
                                       'weight_delta', 'position_delta'])
     num_simulations = 1000
     universes = []
-    for results_before, results_after in zip(ndcgs.keys(), list(ndcgs.keys())[1:]):
-        if results_before == results_after:
+    for results_before_csv, results_after_csv in zip(ndcgs.keys(), list(ndcgs.keys())[1:]):
+        if results_before_csv == results_after_csv:
             continue
-        print(results_before, results_after, num_simulations)
-        mean_ndcg_diff = ndcgs[results_after] - ndcgs[results_before]
+        mean_ndcg_diff = ndcgs[results_after_csv] - ndcgs[results_before_csv]
+        print(results_before_csv, results_after_csv, num_simulations, f"diff: {mean_ndcg_diff:.3f}")
 
-        results_before = pd.read_csv(results_before)
-        results_after = pd.read_csv(results_after)
+        results_before = pd.read_csv(results_before_csv)
+        results_after = pd.read_csv(results_after_csv)
 
         results_diff = create_results_diff(results_before, results_after)
 
@@ -256,9 +291,10 @@ def main():
         new_universes = simulate_at(results_diff, prob_positive,
                                     rounds=num_simulations,
                                     actual_dcg_delta=actual_dcg_delta,
-                                    verbose=False)
+                                    verbose=True)
+        pickle.dump(new_universes, open(f"{os.path.basename(results_after_csv)}_to_{os.path.basename(results_before_csv)}_universes.pkl", "wb"))
 
-        universes = intersect_universes(new_universes, universes, actual_dcg_delta)
+        universes = intersect_universes(new_universes, universes)
 
         # Accumulate judgments from this pair into the evaluation
         # results = results.groupby(['QueryId', 'DocumentId'])[['alpha', 'beta', 'weight_delta', 'position_delta']].sum()
@@ -273,13 +309,15 @@ def main():
     # account for their indpendence (they are not entirely indepnedent)
     # judgments['alpha'] /= math.log(runs)
     # judgments['beta'] /= math.log(runs)
+    import pdb; pdb.set_trace()
+    judgments = universes[0]
 
     # Compute a grade using alpha and beta
     judgments['grade'] = judgments['alpha'] / (judgments['alpha'] + judgments['beta'])
 
     # Join with corpus for debugging
-    corpus = pd.read_csv('vmware_ir_content.csv.zip', compression='zip')
-    queries = pd.read_csv('test.csv')
+    corpus = pd.read_csv('data/vmware_ir_content.csv.zip', compression='zip')
+    queries = pd.read_csv('data/test.csv')
     judgments = judgments.reset_index()
     results = judgments.merge(corpus, right_on='f_name', left_on='DocumentId', how='left')
     results = results.merge(queries, on='QueryId', how='left')
